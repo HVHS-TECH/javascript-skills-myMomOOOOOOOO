3a0cd70db7c7d93da25cdbee2aefada395f2c2c7
LewisBassett888 <24114lb@hvhs.school.nz>
2026-04-30 01:16:00 +0000

    Apr 30, 2026, 1:16 AM

--- t02_variables.js
+++ t02_variables.js
@@ -5,13 +5,17 @@ console.log("Running t02_variables.js")
 console.log('yo wsp little timmys this is a test')
 //variables
 let userName = 'Lewis';
-
+let date = '30th of April 2026';
+let age = '15';
+let PocketMoney = 'a few hundred'
 
 /****************************
 Main code
 ****************************/
 console.log(userName);
-console.log("Your Name Is " + userName);
+console.log("Your name is " + userName);
+console.log("As of " + date) ("you are " + age ("years old " + userName);
+console.log("You have a few hundred dollars" + userName);
 
 
 /****************************
