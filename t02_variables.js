d3e0673f5654d34de21db54984c893e56a034bdc
LewisBassett888 <24114lb@hvhs.school.nz>
2026-04-30 01:04:50 +0000

    Apr 30, 2026, 1:04 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,19 @@
+/****************************
+Task: 1
+****************************/
+console.log("Running t02_variables.js")
+console.log('yo wsp little timmys this is a test')
+//variables
+let username = 
+
+/****************************
+Main code
+****************************/
+
+
+
+
+/****************************
+Functions
+****************************/
 /* Paste your code from task 1 here */
