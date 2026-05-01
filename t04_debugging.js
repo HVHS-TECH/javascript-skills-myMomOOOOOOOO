c316ac46781e78cecd17f75b1747b982bc4f1aea
LewisBassett888 <24114lb@hvhs.school.nz>
2026-05-01 03:05:14 +0000

    May 1, 2026, 3:05 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -18,5 +18,5 @@ console.log("The same piece of paper has a perimeter of "+perimeter)
 if (perimeter > area){
     console.log("Yay, your area is larger, this is right for this example")
 }else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
 }
