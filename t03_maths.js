71d96ad2262b646a65c8d7152ed512367b289fd7
LewisBassett888 <24114lb@hvhs.school.nz>
2026-05-01 02:51:45 +0000

    May 1, 2026, 2:51 AM

--- t03_maths.js
+++ t03_maths.js
@@ -10,15 +10,18 @@ let age = 15;
 let PocketMoney = '500'
 let birth = 2010;
 let future = 10;
+let half = 250;
+let num3 = 3;
 /****************************
 Main code
 ****************************/
 console.log("Hi " + userName);
 console.log("As of " + date + " you are " + age + " years old ");
 console.log("You were born in " + birth);
-console.log("In 10 years you will be ",age + future);
+console.log("In 10 years you will be",age + future);
 console.log("You have " + PocketMoney + " dollars ");
-console.log("")
+console.log("You spend half of your money, now you have",PocketMoney - half + " dollars");
+console.log("Then you get $3, now you have", half + num3 + " dollars ");
 
 
 
