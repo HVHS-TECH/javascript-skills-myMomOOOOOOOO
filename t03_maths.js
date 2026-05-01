b0516652ece78f9174ff9141755b7961c4a5add6
LewisBassett888 <24114lb@hvhs.school.nz>
2026-05-01 02:33:34 +0000

    May 1, 2026, 2:33 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,24 @@
-/* Paste your code from task 2 here */
+/****************************
+Task: 1
+****************************/
+console.log("Running t02_variables.js")
+console.log('yo wsp little timmys this is a test')
+//variables
+let userName = 'Lewis';
+let date = '2026';
+let age = '15';
+let PocketMoney = 'a few hundred'
+
+/****************************
+Main code
+****************************/
+console.log(userName);
+console.log("Your name is " + userName);
+console.log("As of " + date + " you are " + age + " years old ");
+console.log("You have " + PocketMoney + " dollars ");
+
+
+/****************************
+Functions
+****************************/
+/* Paste your code from task 1 here */
