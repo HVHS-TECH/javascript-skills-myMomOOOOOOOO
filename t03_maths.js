19deb9c541b28183ac5af2c549b7fcc774d86fc6
LewisBassett888 <24114lb@hvhs.school.nz>
2026-05-01 02:46:10 +0000

    May 1, 2026, 2:46 AM

--- t03_maths.js
+++ t03_maths.js
@@ -7,7 +7,7 @@ console.log('yo wsp little timmys this is a test')
 let userName = 'Lewis';
 let date = '2026';
 let age = 15;
-let PocketMoney = 'a few hundred'
+let PocketMoney = '500'
 let birth = 2010;
 let future = 10;
 /****************************
@@ -18,7 +18,7 @@ console.log("As of " + date + " you are " + age + " years old ");
 console.log("You were born in " + birth);
 console.log("In 10 years you will be ",age + future);
 console.log("You have " + PocketMoney + " dollars ");
-console.log("Your name is " + userName);
+console.log("")
 
 
 
