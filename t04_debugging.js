1332991844ffedd94ff6ca8d14017e206a0a7ce1
LewisBassett888 <24114lb@hvhs.school.nz>
2026-05-04 21:40:01 +0000

    May 4, 2026, 9:40 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,14 +1,14 @@
 console.log("Running t04_debugging.js")
 
+// Set up the size of the page.
+var length = 8;
+var width = 10;
+
 // calculate the area
 var area = length * width;
 
 // calculate the perimeter
 var perimeter = 2 * length + 2 * width; 
-
-// Set up the size of the page.
-var length = 8;
-var width = 10;
 
 // Display the results
 console.log("The area of a piece of paper with a length of "+length+", and a width of "+width+", is "+area)
