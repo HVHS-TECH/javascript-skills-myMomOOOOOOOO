8ec708dd7dbc8088def079b78c06fd899a91970d
LewisBassett888 <24114lb@hvhs.school.nz>
2026-05-04 21:20:16 +0000

    May 4, 2026, 9:20 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,10 +1,10 @@
 console.log("Running t04_debugging.js")
 
 // calculate the area
-var area = length + width;
+var area = length * width;
 
 // calculate the perimeter
-var perimeter = 2  length + 2 * width; 
+var perimeter = 2 * length + 2 * width; 
 
 // Set up the size of the page.
 var length = 8;
