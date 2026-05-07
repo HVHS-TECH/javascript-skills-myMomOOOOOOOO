a605d1950ee8ffc71bf459948d6c5c36ffe3f030
LewisBassett888 <24114lb@hvhs.school.nz>
2026-05-07 00:53:50 +0000

    May 7, 2026, 12:53 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,28 @@
-/* Paste your code from task 3, Maths here */
+console.log("Running t03_maths.js")
+console.log('yo wsp little timmys this is a test')
+//variables
+let userName = 'Lewis';
+let date = '2026';
+let age = 15;
+let PocketMoney = '500'
+let birth = 2010;
+let future = 10;
+let half = 250;
+let num3 = 3;
+/****************************
+Main code
+****************************/
+console.log("Hi " + userName);
+console.log("As of " + date + " you are " + age + " years old ");
+console.log("You were born in " + birth);
+console.log("In 10 years you will be",age + future);
+console.log("You have " + PocketMoney + " dollars ");
+console.log("You spend half of your money, now you have",PocketMoney - half + " dollars");
+console.log("Then you get $3, now you have", half + num3 + " dollars ");
+
+
+
+
+/****************************
+Functions
+****************************/
