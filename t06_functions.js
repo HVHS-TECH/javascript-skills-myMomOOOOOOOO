a2f3ce25e6eb78c38a034a966790886bdcd2cca6
LewisBassett888 <24114lb@hvhs.school.nz>
2026-05-07 01:10:37 +0000

    May 7, 2026, 1:10 AM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,33 @@
-/* Paste your code from task 5 here */
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+console.log("Running task 05...");
+
+let userName = 'Lewis';
+let date = 2026;
+let age = 15;
+let pocketMoney = 500;
+let birth = 2010;
+let future = 10;
+let half = 250;
+let num3 = 3;
+
+/****************************
+Main code
+****************************/
+
+OUTPUT.innerHTML =
+  "<h2>Added by JavaScript</h2>" +
+  
+  "<p>Hi " + userName + "</p>" +
+
+  "<p>As of " + date + " you are " + age + " years old.</p>" +
+
+  "<p>You were born in " + birth + ".</p>" +
+
+  "<p>In 10 years you will be " + (age + future) + " years old.</p>" +
+
+  "<p>You have $" + pocketMoney + ".</p>" +
+
+  "<p>You spend half of your money, now you have $" + (pocketMoney - half) + ".</p>" +
+
+  "<p>Then you get $3, now you have $" + (half + num3) + ".</p>";
